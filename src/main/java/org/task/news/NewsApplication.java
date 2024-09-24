c6a2b1c03480bcb8b0d50eaa7b137a03e2a7407d
Umar <abduxalilraxmonberdiyev@gmail.com>
2024-09-25 00:36:07 +0500

    feat: spring boot application launches FX library

---
+++ src/main/java/org/task/news/NewsApplication.java
@@ -0,0 +1,20 @@
+package org.task.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class NewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsApplication.class, args);
+        launchApp();
+    }
+
+    private static void launchApp() {
+        NewsApp.launch(NewsApp.class);
+    }
+
+}
